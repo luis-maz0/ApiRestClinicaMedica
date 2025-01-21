a6113399494207f8c7c0debef2599601a18e688b
Luis M <luis.mzo@hotmail.com>
2025-01-21 16:20:16 -0300

    fix nombre método repository & configuracion de Spring security agregado

--- ApiRest/src/main/java/clinica/medica/ApiRest/infra/security/AutenticacionService.java
+++ ApiRest/src/main/java/clinica/medica/ApiRest/infra/security/AutenticacionService.java
@@ -15,6 +15,6 @@ public class AutenticacionService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return usuarioRepository.findByUserName(username);
+        return usuarioRepository.findByLogin(username);
     }
 }
